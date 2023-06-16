e876b12d3e652ff7f49acd5d08707566b4efaf65
Baran Yilmaz <baran.yilmaz12@outlook.com>
2023-06-16 23:07:43 +0200

    more topology functions

--- branch.py
+++ branch.py
@@ -2,11 +2,15 @@ import pandapower as pp
 import networkx as nx
 import pandapower.topology as top
 from collections import Counter
+import pandapower.networks as pn
+import pandapower.plotting as plt
 
 net = pp.from_json("example.json")
 
 
 def find_branch_buses(net):
+    ''' returns the buses which are the first element of a branch with the number of branches as a tuple  
+    '''
     bus_list = list()
     lines = net.line.index
     for line in lines:
@@ -17,23 +21,52 @@ def find_branch_buses(net):
 
 
 def lines_connected_with_bus(net, bus):
+    ''' returns the index of lines which are connected with the bus in a list  
+    '''
+
     lines = net.line.index
     line_index = list()
     for line in lines:
         if net.line.loc[line, "from_bus"] == bus or net.line.loc[line, "to_bus"] == bus:
             line_index.append(line)
 
-    print(line_index)
+    return line_index
 
 
-mg = top.create_nxgraph(net)
-print(nx.shortest_path(mg, 1, 2))
+def get_line_to_bus(net, bus):
+    ''' returns the index of the line which leads to the bus
+    '''
 
-# cc = top.connected_component(mg, 188)
+    lines = net.line.index
+    line_to_bus = None
+    for line in lines:
+        if net.line.loc[line, "to_bus"] == bus:
+            line_to_bus = line
+    return line_to_bus
 
-# for item in cc:
-#    print(item)
-lines_connected_with_bus(net, 1)
-lines_connected_with_bus(net, 414)
-lines_connected_with_bus(net, 413)
-lines_connected_with_bus(net, 415)
+
+def get_lines_from_bus(net, bus):
+    ''' returns the index of the lines which leads away from the bus in a list
+    '''
+    lines_from_bus = list()
+    lines = net.line.index
+    for line in lines:
+        if net.line.loc[line, "from_bus"] == bus:
+            lines_from_bus.append(line)
+    return lines_from_bus
+
+
+def get_prev_bus(net, bus):
+    ''' returns the previous bus of the given bus
+    '''
+
+    line_to_bus = get_line_to_bus(net, bus)
+    prev_bus = net.line.loc[line_to_bus, "from_bus"]
+    return prev_bus
+
+
+def get_main_bus(net, bus):
+    current_bus = bus
+    while (net.line.loc[get_line_to_bus(net, current_bus), "name"] != "starting_ring_line"):
+        current_bus = get_prev_bus(net, current_bus)
+    return current_bus
